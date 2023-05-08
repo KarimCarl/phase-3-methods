ccab75ddd53337117b5214b914e26d6847a8717c
KarimCarl <cndungu109@gmail.com>
2023-05-08 06:09:23 +0300

    Added more notes on Methods and commented original solution

---
+++ methods_my_notes.rb
@@ -0,0 +1,72 @@
+# # Your code here!
+
+# #1
+# def greet_programmer
+#     puts "Hello, programmer!"
+# end
+# greet_programmer 
+
+# #2
+# def greet name 
+#     puts "Hello, #{name}!"
+# end
+# greet "Jimmy"
+
+# #3
+# def greet_with_default (name = "programmer")
+#     puts "Hello, #{name}!"
+# end 
+
+# #4
+# def add (num1, num2)
+#     num1 + num2 
+# end
+
+# #5
+# def halve number
+#     if number.class != Integer
+#         return nil
+#     end
+#     number/2
+# end
+
+#assigning variables in Ruby
+# name = nil
+
+#creating hashes 
+# my_variable = {key1: "value1", key2: "value2"}
+# puts my_variable[:key2]
+
+# def my_method param
+#     "Running my_method"
+#     param + 1
+# end
+# puts (my_method 3)
+
+# def add_and_log(num1, num2)
+#     puts num1 + num2
+# end
+# sum1 = add_and_log(2, 2)
+
+# def add_and_return(num1, num2)
+#     return num1 + num2
+# end
+# add_and_return(2, 2)
+
+# def stylish_painter
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat" #execution ends here due to return keyword
+#     best_hairstyle
+# end 
+# stylish_painter
+
+# def reverse(name)
+#     if name.class != String
+#         return nil
+#     end
+
+#     name.reverse
+# end
+# reverse("Charlie")
+# reverse(163)
+
